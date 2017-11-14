29c0bf65d5ab95d5cee231b0d757808d55de11a6
gskerry <gregory.skerry@gmail.com>
2017-11-14 13:40:58 -0800

    pip installs. whoops.

--- project_setup.sh
+++ project_setup.sh
@@ -5,6 +5,9 @@ echo "Setting up Resonate Project..."
 # Add matplotlibrc to use appropriate backend
 export MATPLOTLIBRC=.matplotlib
 
+# Install python requirements
+pip install -r requirements.txt
+
 # Make sure we have python 3 kernel for jupyter notebook
 ipython3 kernelspec install-self
 
