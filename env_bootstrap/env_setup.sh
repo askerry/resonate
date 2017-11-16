63a0068b7584310377bd959a0b85f540b92db185
gskerry <gregory.skerry@gmail.com>
2017-11-15 22:09:36 -0800

    bootstrap logging

--- env_bootstrap/env_setup.sh
+++ env_bootstrap/env_setup.sh
@@ -4,7 +4,8 @@
 export bootstrap_DIR="$PWD/env_bootstrap"
 # echo "dir: " $bootstrap_DIR
 
-
+echo "PATH: $PATH"
+echo "VIRTUAL_ENV: $VIRTUAL_ENV"
 
 echo "Setting up Resonate Environment..."
 
@@ -13,7 +14,7 @@ virtualenv --python=python3 --no-site-packages ~/.virtualenv/resonate
 echo "pip -V: " $(pip3 -V)
 echo "python -V: " $(python -V)
 
-Activate the virtualenv so that all dependencies are installed within it
+# Activate the virtualenv so that all dependencies are installed within it
 source ~/.virtualenv/resonate/bin/activate
 
 # run project setup scripting
