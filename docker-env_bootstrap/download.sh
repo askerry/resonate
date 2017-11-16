881357dde6d89400f8be3d142f0a936da2b747d8
gskerry <gregory.skerry@gmail.com>
2017-11-16 11:55:04 -0800

    reinstate download script

--- docker-env_bootstrap/download.sh
+++ docker-env_bootstrap/download.sh
@@ -8,23 +8,23 @@ echo "check root: " $ROOT
 
 AUDIO_DIR="$ROOT/raw_data"
 
-# if [ ! -d raw_data ]
-# then
-#     mkdir raw_data
-# fi
-# export AUDIO_DIR="$ROOT/raw_data"
-#
-# cd raw_data
-# if [ ! -f fma_metadata.zip ]
-# then
-#     curl -O https://os.unil.cloud.switch.ch/fma/fma_metadata.zip
-#     7za x fma_metadata.zip
-# fi
-#
-# if [ ! -f fma_small.zip ]
-# then
-#     curl -O https://os.unil.cloud.switch.ch/fma/fma_small.zip
-#     7za x fma_small.zip
-# fi
+if [ ! -d raw_data ]
+then
+    mkdir raw_data
+fi
+export AUDIO_DIR="$ROOT/raw_data"
+
+cd raw_data
+if [ ! -f fma_metadata.zip ]
+then
+    curl -O https://os.unil.cloud.switch.ch/fma/fma_metadata.zip
+    7za x fma_metadata.zip
+fi
+
+if [ ! -f fma_small.zip ]
+then
+    curl -O https://os.unil.cloud.switch.ch/fma/fma_small.zip
+    7za x fma_small.zip
+fi
 
 echo "Data downloaded to" $AUDIO_DIR
